f78677a7943aadb757fc80e81e889a8a38fd72b0
pushpakdeore09 <pushpakdeore9114@gmail.com>
2025-03-01 20:11:50 +0530

    changesmade

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -15,7 +15,7 @@ export class ContentComponent implements OnChanges {
   content: string = '';
 
   images = [
-    { url: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHVybHxlbnwwfHwwfHx8MA%3D%3D', altText: 'Image 1' },
+    
     { url: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHVybHxlbnwwfHwwfHx8MA%3D%3D', altText: 'Image 3' },
     { url: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHVybHxlbnwwfHwwfHx8MA%3D%3D', altText: 'Image 4' }
   ];
