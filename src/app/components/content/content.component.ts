1230ca519b54110fe363568c9015336a4e3e0d01
pushpakdeore09 <pushpakdeore9114@gmail.com>
2025-03-01 23:04:21 +0530

    apiadded

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -14,13 +14,6 @@ export class ContentComponent implements OnChanges {
   @Input() selectedSubSubCategory!: string;
   content: string = '';
 
-  images = [
-    
-    { url: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHVybHxlbnwwfHwwfHx8MA%3D%3D', altText: 'Image 3' },
-    { url: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHVybHxlbnwwfHwwfHx8MA%3D%3D', altText: 'Image 4' }
-  ];
-  
-
   constructor(private contentService: ContentService) {}
 
   ngOnChanges(changes: SimpleChanges): void {
--- src/app/service/content/content.service.ts
+++ src/app/service/content/content.service.ts
@@ -1,28 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor() { }
+  url = 'http://localhost:8080/api'
+
+  constructor(private http: HttpClient) { }
   
-  getHtmlContent(filepath: string): Promise<string> {
-    console.log('Loading content:', filepath);
-    
-    return fetch(`/content/${filepath}.html`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('File not found');
-        }
-        return response.text();
-      })
-      .catch((error) => {
-        console.error('Error loading file:', error);
-        return '<p>Content not found</p>'; 
-      });
+  addResource(resource: any) {
+    return this.http.post(this.url + '/add-resource', { resource });
   }
   
 }
