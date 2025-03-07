500737c4c025ccd9da619360a526fce242217be0
pushpakdeore09 <pushpakdeore9114@gmail.com>
2025-03-07 23:02:09 +0530

    changesmade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,34 @@ import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContentComponent } from './components/content/content.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+  showPopup = false;
+  constructor() {}
+
+  ngOnInit(){
+    this.checkViewPortSize();
+  }
+
+  checkViewPortSize(){
+
+    const viewportWidth = window.innerWidth;
+    console.log('Viewport width:', viewportWidth, 'px'); 
+    if(window.innerWidth < 768){
+      this.showPopup = true;
+    }
+  }
+
+  closePopup(){
+    this.showPopup = false;
+  }
   
 }
